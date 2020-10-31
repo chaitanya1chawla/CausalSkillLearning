bd6fcaebdfd1cbf031b7e65a10ec2b3d55541349
Tanmay Shankar <tshankar@andrew.cmu.edu>
2020-10-31 01:21:33 -0400

    Updating to new style of file writer.

--- Experiments/TFLogger.py
+++ Experiments/TFLogger.py
@@ -54,8 +54,8 @@ class Logger(object):
     def __init__(self, log_dir):
         """Create a summary writer logging to log_dir."""
         # Switching to TF 2.2 implementation.
-        self.writer = tf.summary.create_file_writer(log_dir)
-        # self.writer = tf.summary.FileWriter(log_dir)
+        # self.writer = tf.summary.create_file_writer(log_dir)
+        self.writer = tf.summary.FileWriter(log_dir)
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
