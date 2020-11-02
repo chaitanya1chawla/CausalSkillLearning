43dc8794ac2b4dded6a27e28a93f5054224ac4ad
Tanmay Shankar <tshankar@andrew.cmu.edu>
2020-11-02 00:01:48 -0500

    Indent issues

--- Experiments/PolicyManagers.py
+++ Experiments/PolicyManagers.py
@@ -1184,8 +1184,8 @@ class PolicyManager_BatchPretrain(PolicyManager_Pretrain):
 
 			data_element = self.dataset[i:i+self.args.batch_size]
 
-            # Must select common trajectory segment length for batch.
-            # Must select different start / end points for items of batch?
+			# Must select common trajectory segment length for batch.
+			# Must select different start / end points for items of batch?
 
 			# If allowing variable skill length, set length for this sample.				
 			if self.args.var_skill_length:
@@ -1193,50 +1193,50 @@ class PolicyManager_BatchPretrain(PolicyManager_Pretrain):
 				self.current_traj_len = np.random.choice([12,13,14,15,16],p=[0.1,0.2,0.4,0.2,0.1])
 			else:
 				self.current_traj_len = self.traj_length            
-            
+			
 			batch_trajectory = np.zeros((self.args.batch_size, self.current_traj_len, self.state_size))
 
 			for x in range(self.args.batch_size):
-                
-                # Select the trajectory for each instance in the batch. 
-                traj = data_element[x]['demo']
+				
+				# Select the trajectory for each instance in the batch. 
+				traj = data_element[x]['demo']
 
-                # Pick start and end.               
+				# Pick start and end.               
 
-                # Sample random start point.
-                if traj.shape[0]>self.current_traj_len:
+				# Sample random start point.
+				if traj.shape[0]>self.current_traj_len:
 
-                    bias_length = int(self.args.pretrain_bias_sampling*traj.shape[0])
+					bias_length = int(self.args.pretrain_bias_sampling*traj.shape[0])
 
-                    # Probability with which to sample biased segment: 
-                    sample_biased_segment = np.random.binomial(1,p=self.args.pretrain_bias_sampling_prob)
+					# Probability with which to sample biased segment: 
+					sample_biased_segment = np.random.binomial(1,p=self.args.pretrain_bias_sampling_prob)
 
-                    # If we want to bias sampling of trajectory segments towards the middle of the trajectory, to increase proportion of trajectory segments
-                    # that are performing motions apart from reaching and returning. 
+					# If we want to bias sampling of trajectory segments towards the middle of the trajectory, to increase proportion of trajectory segments
+					# that are performing motions apart from reaching and returning. 
 
-                    # Sample a biased segment if trajectory length is sufficient, and based on probability of sampling.
-                    if ((traj.shape[0]-2*bias_length)>self.current_traj_len) and sample_biased_segment:		
-                        start_timepoint = np.random.randint(bias_length, traj.shape[0] - self.current_traj_len - bias_length)
-                    else:
-                        start_timepoint = np.random.randint(0,traj.shape[0]-self.current_traj_len)
+					# Sample a biased segment if trajectory length is sufficient, and based on probability of sampling.
+					if ((traj.shape[0]-2*bias_length)>self.current_traj_len) and sample_biased_segment:		
+						start_timepoint = np.random.randint(bias_length, traj.shape[0] - self.current_traj_len - bias_length)
+					else:
+						start_timepoint = np.random.randint(0,traj.shape[0]-self.current_traj_len)
 
-                    end_timepoint = start_timepoint + self.current_traj_len
+					end_timepoint = start_timepoint + self.current_traj_len
 
-                    batch_trajectory[x] = data_element[x]['demo'][start_timepoint:end_timepoint]
-                    if not(self.args.gripper):
-                    	batch_trajectory[x] = data_element['demo'][start_timepoint:end_timepoint,:-1]
+					batch_trajectory[x] = data_element[x]['demo'][start_timepoint:end_timepoint]
+					if not(self.args.gripper):
+						batch_trajectory[x] = data_element['demo'][start_timepoint:end_timepoint,:-1]
 
-            # If normalization is set to some value.
-            if self.args.normalization=='meanvar' or self.args.normalization=='minmax':
-                batch_trajectory = (batch_trajectory-self.norm_sub_value)/self.norm_denom_value
+			# If normalization is set to some value.
+			if self.args.normalization=='meanvar' or self.args.normalization=='minmax':
+				batch_trajectory = (batch_trajectory-self.norm_sub_value)/self.norm_denom_value
 
-            # Compute actions.
+			# Compute actions.
 			action_sequence = np.diff(batch_trajectory,axis=1)
 
 			# Concatenate
 			concatenated_traj = self.concat_state_action(batch_trajectory, action_sequence)
 
-            # Scaling action sequence by some factor.             
+			# Scaling action sequence by some factor.             
 			scaled_action_sequence = self.args.action_scale_factor*action_sequence
 
 			return concatenated_traj, scaled_action_sequence, batch_trajectory
@@ -1328,7 +1328,7 @@ class PolicyManager_BatchPretrain(PolicyManager_Pretrain):
 
 	def assemble_inputs(self, input_trajectory, latent_z_indices, latent_b, sample_action_seq):
 
-        if not(self.args.discrete_z):
+		if not(self.args.discrete_z):
 			# # Append latent z indices to sample_traj data to feed as input to BOTH the latent policy network and the subpolicy network. 
 			# assembled_inputs = torch.zeros((len(input_trajectory),self.input_size+self.latent_z_dimensionality+1)).to(device)
 			# assembled_inputs[:,:self.input_size] = torch.tensor(input_trajectory).view(len(input_trajectory),self.input_size).to(device).float()			
@@ -1337,22 +1337,22 @@ class PolicyManager_BatchPretrain(PolicyManager_Pretrain):
 			# assembled_inputs[range(1,len(input_trajectory)),-1] = latent_b[:-1].float()
 
 			# Now assemble inputs for subpolicy.
-            
-            # Create subpolicy inputs tensor. 
+			
+			# Create subpolicy inputs tensor. 
 			# subpolicy_inputs = torch.zeros((len(input_trajectory),self.input_size+self.latent_z_dimensionality)).to(device)
-            subpolicy_inputs = torch.zeros((len(input_trajectory), self.args.batch_size, self.input_size+self.latent_z_dimensionality)).to(device)
+			subpolicy_inputs = torch.zeros((len(input_trajectory), self.args.batch_size, self.input_size+self.latent_z_dimensionality)).to(device)
 
-            # Now copy over trajectory. 
+			# Now copy over trajectory. 
 			# subpolicy_inputs[:,:self.input_size] = torch.tensor(input_trajectory).view(len(input_trajectory),self.input_size).to(device).float()         
-            subpolicy_inputs[:,:,:self.input_size] = torch.tensor(input_trajectory).view(len(input_trajectory),self.args.batch_size,self.input_size).to(device).float()
+			subpolicy_inputs[:,:,:self.input_size] = torch.tensor(input_trajectory).view(len(input_trajectory),self.args.batch_size,self.input_size).to(device).float()
 
-            # Now copy over latent z's. 
+			# Now copy over latent z's. 
 			subpolicy_inputs[range(len(input_trajectory)),:,self.input_size:] = latent_z_indices
 
 			# # Concatenated action sequence for policy network's forward / logprobabilities function. 
 			# padded_action_seq = np.concatenate([np.zeros((1,self.output_size)),sample_action_seq],axis=0)
-            # View time first and batch second for downstream LSTM.
-            sample_action_seq = sample_action_seq.transpose((1,0,2))
+			# View time first and batch second for downstream LSTM.
+			sample_action_seq = sample_action_seq.transpose((1,0,2))
 			padded_action_seq = np.concatenate([sample_action_seq,np.zeros((1,self.args.batch_size,self.output_size))],axis=0)
 
 			return None, subpolicy_inputs, padded_action_seq
