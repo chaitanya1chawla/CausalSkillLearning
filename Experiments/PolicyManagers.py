df349cb38bb3c5a17c16e50f2abdc1365d6c0695
Tanmay Shankar <tshankar@andrew.cmu.edu>
2020-11-02 15:38:10 -0500

    Reducing zoom and embedding size further

--- Experiments/PolicyManagers.py
+++ Experiments/PolicyManagers.py
@@ -468,9 +468,10 @@ class PolicyManager_BaseClass():
 
 		# Create figure and axis objects.
 		# matplotlib.rcParams['figure.figsize'] = [50, 50]
-		matplotlib.rcParams['figure.figsize'] = [20, 20]
+		# matplotlib.rcParams['figure.figsize'] = [20, 20]
+		matplotlib.rcParams['figure.figsize'] = [10, 10]
 		# zoom_factor = 0.4
-		zoom_factor = 0.15
+		zoom_factor = 0.1
 		fig, ax = plt.subplots()
 
 		# number_samples = 400
