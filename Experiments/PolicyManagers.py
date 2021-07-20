84bd04b03a07a36404e406e7874ee104b52efaf1
Tanmay Shankar <tanmay.shankar@gmail.com>
2021-07-20 14:03:49 -0400

    Fixing low ll skills viz

--- Experiments/PolicyManagers.py
+++ Experiments/PolicyManagers.py
@@ -9100,6 +9100,8 @@ class PolicyManager_JointFixEmbedTransfer(PolicyManager_Transfer):
 				traj = input_dict['sample_traj'][indices]
 				unnorm_traj = (traj*self.source_manager.norm_denom_value) + self.source_manager.norm_sub_value		
 
+				print("embedding in visualize low ll skillss")
+				embed()
 				# Now visualize.. 
 				self.visualizer.visualize_joint_trajectory(unnorm_traj, gif_path="LowlikelihoodTraj", gif_name="Unaligned_Traj{0}.npy".format(self.global_traj_counter), return_and_save=True)
 
