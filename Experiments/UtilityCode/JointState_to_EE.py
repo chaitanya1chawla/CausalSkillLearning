ed57680671d7017eaa11592b4ed1605aa26df25d
Tanmay Shankar <tanmay.shankar@gmail.com>
2021-07-23 15:53:45 -0400

    Logging new changes to end effector state computation.

--- Experiments/UtilityCode/JointState_to_EE.py
+++ Experiments/UtilityCode/JointState_to_EE.py
@@ -3,6 +3,7 @@
 from Visualizers import BaxterVisualizer
 import numpy as np, robosuite, copy
 
+
 x = np.load("MIMEDataArray.npy",allow_pickle=True)
 
 # Create visualizer object that we are going to use to interact with the environment.. 
@@ -23,16 +24,40 @@ for k, v in enumerate(x):
 		# Get joint state. 
 		joint_state = v['demo'][t]
 
-		# Set joint state. 
-		visualizer.set_joint_pose(joint_state)
-		visualizer.update_state()
-		
-		# Get EE position.
-		obs = copy.deepcopy(visualizer.full_state)
+		# # Set joint state. 
+		# visualizer.set_joint_pose(joint_state)
+		# visualizer.update_state()
 
+		# Get EE position.
+		# obs = copy.deepcopy(visualizer.full_state)
 		# Assembly.
 		# COncatenating in order: right ee pos, right ee q, left ee pos, left ee q.
-		ee_state = np.concatenate([obs['right_eef_pos'], obs['right_eef_quat'], obs['left_eef_pos'], obs['left_eef_quat'], joint_state[-2:]])
+		# ee_state = np.concatenate([obs['right_eef_pos'], obs['right_eef_quat'], obs['left_eef_pos'], obs['left_eef_quat'], joint_state[-2:]])
+
+		# Instead of using env... 
+		# Use...
+		visualizer.baxter_IK_object.controller.sync_ik_robot(joint_state[:-2])
+		ee_state = np.concatenate(visualizer.baxter_IK_object.controller.ik_robot_eef_joint_cartesian_pose())
+
+		# # Can verify this EE state is okay by running it through IK. 
+		# ee_pose = ee_state
+		# seed = np.random.random(14)
+		# ik_joint_state = np.array(visualizer.baxter_IK_object.controller.inverse_kinematics(
+		# 		target_position_right=ee_pose[:3],
+		# 		target_orientation_right=ee_pose[3:7],
+		# 		target_position_left=ee_pose[7:10],
+		# 		target_orientation_left=ee_pose[10:],
+		# 		rest_poses=seed))
+					
+		# norm = np.linalg.norm(joint_state[:-2] - ik_joint_state)
+		# if norm > 0.001:
+		# 	# print("Embedding in IK")
+		# 	print("Issue at: ", k, t, norm)
+		# 	embed()
+		
+		# Assembly.	
+		# COncatenating in order: right ee pos, right ee q, left ee pos, left ee q, gripper.. 
+		ee_state = np.concatenate([ee_state, joint_state[-2:]])
 		
 		if ee_traj is None:
 			ee_traj = ee_state.reshape(1,-1)
@@ -40,6 +65,8 @@ for k, v in enumerate(x):
 			ee_traj = np.concatenate([ee_traj, ee_state.reshape(1,-1)],axis=0)
 
 	v['endeffector_trajectory'] = ee_traj
+
+np.save("MIMEDataArray_EEAugmented.npy",x)
 
 # js = x[0]['demo'][20]
 
