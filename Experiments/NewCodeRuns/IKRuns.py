0c25f98fbf19bfb08e40bf3f02122026fa3cb366
Tanmay Shankar <tanmay.shankar@gmail.com>
2021-07-28 11:34:50 -0400

    Logging changes

--- Experiments/NewCodeRuns/IKRuns.py
+++ Experiments/NewCodeRuns/IKRuns.py
@@ -17,3 +17,10 @@ python cluster_run.py --name='IK_005' --cmd='python Master.py --train=1 --settin
 
 # Play eval
 python Master.py --train=1 --setting=iktrainer --name=IK_004_Eval --data=MIME --number_layers=4 --hidden_size=48 --batch_size=32 --eval_freq=500 --seed=0 --datadir=/private/home/tanmayshankar/Research/Code/Data/Datasets/MIME/ --short_trajectories=1 --model=ExpWandbLogs/IK_004/saved_models/Model_epoch500 --debug=1
+
+# Rerunning after fixing stupid mistake
+python cluster_run.py --name='IK_010' --cmd='python Master.py --train=1 --setting=iktrainer --name=IK_010 --data=MIME --number_layers=4 --hidden_size=48 --batch_size=32 --eval_freq=500 --seed=0 --datadir=/private/home/tanmayshankar/Research/Code/Data/Datasets/MIME/ --short_trajectories=1'
+
+python cluster_run.py --name='IK_011' --cmd='python Master.py --train=1 --setting=iktrainer --name=IK_011 --data=MIME --number_layers=4 --hidden_size=48 --batch_size=32 --eval_freq=500 --seed=1 --datadir=/private/home/tanmayshankar/Research/Code/Data/Datasets/MIME/ --short_trajectories=1'
+
+python cluster_run.py --name='IK_012' --cmd='python Master.py --train=1 --setting=iktrainer --name=IK_012 --data=MIME --number_layers=4 --hidden_size=48 --batch_size=32 --eval_freq=500 --seed=2 --datadir=/private/home/tanmayshankar/Research/Code/Data/Datasets/MIME/ --short_trajectories=1'
