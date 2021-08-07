27f178a9ddc055f3cac07c0efc9dd0ab5674640a
Tanmay Shankar <tanmay.shankar@gmail.com>
2021-08-07 12:03:07 -0400

    Changing mean value

--- Experiments/Visualizers.py
+++ Experiments/Visualizers.py
@@ -174,7 +174,8 @@ class BaxterVisualizer():
 				# seed = np.ones(14)/2
 
 				# Mean position
-				mean_position = np.array([ 0.43,  0.48, -1.87,  0.94, -2.01, -1.44,  1.54, -0.41,  0.41, 1.57,  1.29, -1.15,  1.08,  1.69])
+				# mean_position = np.array([ 0.43,  0.48, -1.87,  0.94, -2.01, -1.44,  1.54, -0.41,  0.41, 1.57,  1.29, -1.15,  1.08,  1.69])
+				mean_position = np.array([ 0.21,  0.2 , -1.26,  1.28, -0.96,  0.13,  0.  , -0.3 ,  0.06, 1.33,  1.29,  0.01,  0.26, -0.01])
 				seed = mean_position
 
 			# The rest poses / seed only makes a difference when you make the IK_object's controller state get set to this seed....
