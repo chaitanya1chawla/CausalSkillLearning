a07beef7aff7a0870dbbdc6f56579b911cc3ca1b
tanmayshankar <tanmay.shankar@gmail.com>
2021-08-31 15:14:22 -0400

    Logging runs

--- Experiments/NewCodeRuns/NewRoboturkPretrainRuns.py
+++ Experiments/NewCodeRuns/NewRoboturkPretrainRuns.py
@@ -150,6 +150,11 @@ python Master.py --train=0 --setting=pretrain_sub --name=RTP_051_eval_p10_m335 -
 
 CUDA_VISIBLE_DEVICES=1 python Master.py --train=0 --setting=pretrain_sub --name=RTP_052_eval_p10_m335 --data=FullRoboturk --kl_weight=0.0001 --var_skill_length=1 --z_dimensions=16 --number_layers=4 --hidden_size=48 --batch_size=32 --normalization=minmax --no_mujoco=0 --seed=0 --epsilon_from=0.3 --epsilon_to=0.1 --epsilon_over=100 --display_freq=2000 --epochs=2000 --datadir=/private/home/tanmayshankar/Research/Code/Data/Datasets/Roboturk/ --model=ExpWandbLogs/RTP_052/saved_models/Model_epoch335 --perplexity=10
 
+# 
+# Eval 50 series
+python Master.py --train=0 --setting=pretrain_sub --name=RTP_051_eval_p10 --data=FullRoboturk --kl_weight=0.0001 --var_skill_length=1 --z_dimensions=16 --number_layers=4 --hidden_size=48 --batch_size=32 --normalization=minmax --no_mujoco=0 --seed=0 --epsilon_from=0.3 --epsilon_to=0.1 --epsilon_over=100 --display_freq=2000 --epochs=2000 --datadir=/home/tshankar/Research/Code/Data/Datasets/Roboturk/ --model=ExpWandbLogs/RTP_051/saved_models/Model_epoch340 --perplexity=10 --debug=1
+
+
 ######################################################
 ######################################################
 
