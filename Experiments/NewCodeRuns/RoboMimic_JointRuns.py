704f3c3e1cfdc36e6b18bc0d3cff75c815112e91
tanmayshankar <tanmay.shankar@gmail.com>
2022-01-05 12:55:50 -0500

    Adding run logs

--- Experiments/NewCodeRuns/RoboMimic_JointRuns.py
+++ Experiments/NewCodeRuns/RoboMimic_JointRuns.py
@@ -22,3 +22,18 @@ python Master.py --train=1 --setting=learntsub --name=RM_002_cont1 --normalizati
 
 # Rerun with the visualization
 python Master.py --train=1 --setting=learntsub --name=RM_testviz --normalization=meanvar --kl_weight=0.001 --subpolicy_ratio=1. --latentpolicy_ratio=0.0 --b_probability_factor=0.01 --data=RoboMimic  --model=ExpWandbLogs/RM_002/saved_models/Model_epoch1000 --latent_loss_weight=0.0 --z_dimensions=16 --traj_length=-1 --var_skill_length=1 --training_phase_size=2000 --number_layers=4 --hidden_size=48 --var_number_layers=5 --var_hidden_size=48 --batch_size=32 --seed=2 --short_trajectories=1 --epsilon_from=0.3 --epsilon_to=0.01 --epsilon_over=200 --epochs=2000 --normalization=meanvar
+
+# Run joint RM training with 010 and 011 models.
+# RMP_010 - minmax, RMP_011 - meanvar
+#
+CUDA_VISIBLE_DEVICES=2 python Master.py --train=1 --setting=learntsub --name=RM_003 --normalization=meanvar --kl_weight=0.001 --subpolicy_ratio=1. --latentpolicy_ratio=0.0 --b_probability_factor=0.01 --data=RoboMimic  --subpolicy_model=ExpWandbLogs/RMP_010/saved_models/Model_epoch5000 --latent_loss_weight=0.0 --z_dimensions=16 --traj_length=-1 --var_skill_length=1 --training_phase_size=2000 --number_layers=4 --hidden_size=48 --var_number_layers=5 --var_hidden_size=48 --batch_size=32 --seed=2 --short_trajectories=1 --epsilon_from=0.3 --epsilon_to=0.01 --epsilon_over=200 --epochs=5000 --normalization=minmax --save_freq=100
+
+# 
+CUDA_VISIBLE_DEVICES=2 python Master.py --train=1 --setting=learntsub --name=RM_004 --normalization=meanvar --kl_weight=0.001 --subpolicy_ratio=1. --latentpolicy_ratio=0.0 --b_probability_factor=0.01 --data=RoboMimic  --subpolicy_model=ExpWandbLogs/RMP_011/saved_models/Model_epoch5000 --latent_loss_weight=0.0 --z_dimensions=16 --traj_length=-1 --var_skill_length=1 --training_phase_size=2000 --number_layers=4 --hidden_size=48 --var_number_layers=5 --var_hidden_size=48 --batch_size=32 --seed=2 --short_trajectories=1 --epsilon_from=0.3 --epsilon_to=0.01 --epsilon_over=200 --epochs=5000 --normalization=meanvar --save_freq=100
+
+# Also running on hinton for speed check
+#
+CUDA_VISIBLE_DEVICES=0 python Master.py --train=1 --setting=learntsub --name=RM_005 --normalization=meanvar --kl_weight=0.001 --subpolicy_ratio=1. --latentpolicy_ratio=0.0 --b_probability_factor=0.01 --data=RoboMimic  --subpolicy_model=ExpWandbLogs/RMP_010/saved_models/Model_epoch5000 --latent_loss_weight=0.0 --z_dimensions=16 --traj_length=-1 --var_skill_length=1 --training_phase_size=2000 --number_layers=4 --hidden_size=48 --var_number_layers=5 --var_hidden_size=48 --batch_size=32 --seed=2 --short_trajectories=1 --epsilon_from=0.3 --epsilon_to=0.01 --epsilon_over=200 --epochs=5000 --normalization=minmax --save_freq=100
+
+# 
+CUDA_VISIBLE_DEVICES=0 python Master.py --train=1 --setting=learntsub --name=RM_006 --normalization=meanvar --kl_weight=0.001 --subpolicy_ratio=1. --latentpolicy_ratio=0.0 --b_probability_factor=0.01 --data=RoboMimic  --subpolicy_model=ExpWandbLogs/RMP_011/saved_models/Model_epoch5000 --latent_loss_weight=0.0 --z_dimensions=16 --traj_length=-1 --var_skill_length=1 --training_phase_size=2000 --number_layers=4 --hidden_size=48 --var_number_layers=5 --var_hidden_size=48 --batch_size=32 --seed=2 --short_trajectories=1 --epsilon_from=0.3 --epsilon_to=0.01 --epsilon_over=200 --epochs=5000 --normalization=meanvar --save_freq=100
--- Experiments/NewCodeRuns/RoboMimic_PretrainRuns.py
+++ Experiments/NewCodeRuns/RoboMimic_PretrainRuns.py
@@ -28,4 +28,27 @@ CUDA_VISIBLE_DEVICES=1 python Master.py --train=1 --setting=pretrain_sub --name=
 # Trial
 python Master.py --train=1 --setting=pretrain_sub --name=RMP_trial --data=RoboMimic --number_layers=4 --hidden_size=48 --kl_weight=0.001 --var_skill_length=1 --z_dimensions=16 --batch_size=32 --eval_freq=500 --seed=0 --epsilon_from=0.1 --epsilon_to=0.01 --epsilon_over=200 --display_freq=2000 --action_scale_factor=1 --normalization=minmax --epochs=2000
 
-# 
+# Visualize RMP_007
+CUDA_VISIBLE_DEVICES=1 python Master.py --train=0 --setting=pretrain_sub --name=RMP_007_Eval2 --data=RoboMimic --number_layers=4 --hidden_size=48 --kl_weight=0.001 --var_skill_length=1 --z_dimensions=16 --batch_size=32 --eval_freq=500 --seed=0 --epsilon_from=0.1 --epsilon_to=0.01 --epsilon_over=200 --display_freq=2000 --action_scale_factor=1 --normalization=meanvar --epochs=2000 --model=ExpWandbLogs/RMP_007/saved_models/Model_epoch2000
+
+# Viz RMP_005
+python Master.py --train=0 --setting=pretrain_sub --name=RMP_005_Eval2 --data=RoboMimic --number_layers=4 --hidden_size=48 --kl_weight=0.001 --var_skill_length=1 --z_dimensions=16 --batch_size=32 --eval_freq=500 --seed=0 --epsilon_from=0.1 --epsilon_to=0.01 --epsilon_over=200 --display_freq=2000 --action_scale_factor=1 --normalization=minmax --epochs=2000 --model=ExpWandbLogs/RMP_005/saved_models/Model_epoch2000
+
+#### 
+# Running for longer.
+python Master.py --train=1 --setting=pretrain_sub --name=RMP_010 --data=RoboMimic --number_layers=4 --hidden_size=48 --kl_weight=0.001 --var_skill_length=1 --z_dimensions=16 --batch_size=32 --eval_freq=500 --seed=0 --epsilon_from=0.1 --epsilon_to=0.01 --epsilon_over=200 --display_freq=2000 --action_scale_factor=1 --normalization=minmax --epochs=5000 --save_freq=100
+
+python Master.py --train=1 --setting=pretrain_sub --name=RMP_011 --data=RoboMimic --number_layers=4 --hidden_size=48 --kl_weight=0.001 --var_skill_length=1 --z_dimensions=16 --batch_size=32 --eval_freq=500 --seed=0 --epsilon_from=0.1 --epsilon_to=0.01 --epsilon_over=200 --display_freq=2000 --action_scale_factor=1 --normalization=meanvar --epochs=5000 --save_freq=100
+
+# Rerun 005 / 007 for longer as well
+CUDA_VISIBLE_DEVICES=1 python Master.py --train=1 --setting=pretrain_sub --name=RMP_012 --data=RoboMimic --number_layers=4 --hidden_size=48 --kl_weight=0.001 --var_skill_length=1 --z_dimensions=16 --batch_size=32 --eval_freq=500 --seed=0 --epsilon_from=0.1 --epsilon_to=0.01 --epsilon_over=200 --display_freq=2000 --action_scale_factor=1 --normalization=meanvar --epochs=2000 --model=ExpWandbLogs/RMP_007/saved_models/Model_epoch2000 --save_freq=100
+
+CUDA_VISIBLE_DEVICES=1 python Master.py --train=1 --setting=pretrain_sub --name=RMP_013 --data=RoboMimic --number_layers=4 --hidden_size=48 --kl_weight=0.001 --var_skill_length=1 --z_dimensions=16 --batch_size=32 --eval_freq=500 --seed=0 --epsilon_from=0.1 --epsilon_to=0.01 --epsilon_over=200 --display_freq=2000 --action_scale_factor=1 --normalization=minmax --epochs=2000 --model=ExpWandbLogs/RMP_005/saved_models/Model_epoch2000 --save_freq=100
+
+# Evaluate RMP_010 and 011
+python Master.py --train=0 --setting=pretrain_sub --name=RMP_010_Eval --data=RoboMimic --number_layers=4 --hidden_size=48 --kl_weight=0.001 --var_skill_length=1 --z_dimensions=16 --batch_size=32 --eval_freq=500 --seed=0 --epsilon_from=0.1 --epsilon_to=0.01 --epsilon_over=200 --display_freq=2000 --action_scale_factor=1 --normalization=minmax --epochs=5000 --save_freq=100 --model=ExpWandbLogs/RMP_010/saved_models/Model_epoch5000
+
+python Master.py --train=0 --setting=pretrain_sub --name=RMP_011_Eval --data=RoboMimic --number_layers=4 --hidden_size=48 --kl_weight=0.001 --var_skill_length=1 --z_dimensions=16 --batch_size=32 --eval_freq=500 --seed=0 --epsilon_from=0.1 --epsilon_to=0.01 --epsilon_over=200 --display_freq=2000 --action_scale_factor=1 --normalization=meanvar --epochs=5000 --save_freq=100 --model=ExpWandbLogs/RMP_011/saved_models/Model_epoch5000
+
+# play with dataset
+python Master.py --train=1 --setting=pretrain_sub --name=RMP_play --data=RoboMimic --number_layers=4 --hidden_size=48 --kl_weight=0.001 --var_skill_length=1 --z_dimensions=16 --batch_size=32 --eval_freq=500 --seed=0 --epsilon_from=0.1 --epsilon_to=0.01 --epsilon_over=200 --display_freq=2000 --action_scale_factor=1 --normalization=meanvar --epochs=5000 --save_freq=100
