ba7b1e8b11c00e75d47dd727118c8d76bc2c25ee
almutwakel <almutwakelhassan@gmail.com>
2022-07-14 21:44:38 -0400

    Update GRAB_DataLoader.py

--- Experiments/GRAB_DataLoader.py
+++ Experiments/GRAB_DataLoader.py
@@ -304,7 +304,7 @@ class GRAB_PreDataset(Dataset):
 			# Reshape. 
 			reshaped_normalized_datapoint = normalized_relevant_joint_datapoint.reshape(normalized_relevant_joint_datapoint.shape[0],-1)
 
-			self.state_size = len(reshaped_normalized_datapoint) * 3
+			self.state_size = len(reshaped_normalized_datapoint[0]) * 3
 
 			# Subsample in time. 
 			number_of_timesteps = datapoint.shape[0]//self.ds_freq
