a628e816d16be26441fbe95bd548c1c6e2a1b7c6
almutwakel <almutwakelhassan@gmail.com>
2022-10-02 02:25:57 -0400

    Update DAPG_DataLoader.py

--- Experiments/DAPG_DataLoader.py
+++ Experiments/DAPG_DataLoader.py
@@ -32,7 +32,7 @@ class DAPG_PreDataset(Dataset):
 		if self.args.datadir is None:
 			# self.dataset_directory = '/checkpoint/tanmayshankar/MIME/'
 			# self.dataset_directory = '/home/tshankar/Research/Code/Data/Datasets/MIME/'
-			self.dataset_directory = '/home/ahassan/CausalSkillLearning/dapg/hand_dapg/dapg/demonstrations/'
+			self.dataset_directory = '/home/ahassan/CausalSkillLearning/Experiments/dapg/hand_dapg/dapg/demonstrations/'
 		else:
 			self.dataset_directory = self.args.datadir
 		   
