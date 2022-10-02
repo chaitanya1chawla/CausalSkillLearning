c6f08fe49a8a3a4540d95f73df54f621400570aa
almutwakel <almutwakelhassan@gmail.com>
2022-10-02 02:35:09 -0400

    Update Visualizers.py

--- Experiments/Visualizers.py
+++ Experiments/Visualizers.py
@@ -843,7 +843,7 @@ class GRABArmHandVisualizer(GRABVisualizer):
 class DAPGVisualizer(SawyerVisualizer):
 		
 	def __init__(self, args=None):
-		super.__init__(self)
+		super().__init__()
 
 	def set_joint_pose_return_image(self, joint_angles, arm='both', gripper=False, save_image=False):
 		environment = GymEnv("relocate-v0")
