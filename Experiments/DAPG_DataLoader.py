9afa9d124411b3685a609b9b9ed0713b9611d9a8
almutwakel <almutwakelhassan@gmail.com>
2023-03-12 23:17:09 -0400

    'task_id' -> 'task-id'

--- Experiments/DAPG_DataLoader.py
+++ Experiments/DAPG_DataLoader.py
@@ -325,7 +325,7 @@ class DAPG_Dataset(Dataset):
 		# task_index = np.searchsorted(self.cumulative_num_demos, index, side='right')-1
 		# data_element['file'] = self.filelist[task_index][81:-7]
 		data_element['file'] = self.environment_names[index]
-		data_element['task_id'] = index
+		data_element['task-id'] = index
 		# print("Printing the index and the task ID from dataset:", index, data_element['file'])
 
 		return data_element
