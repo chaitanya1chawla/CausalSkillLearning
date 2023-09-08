24f3b68d9f84650cc791eff05d48c5e13da262aa
cmuroboticsdrl <cmu.robotics.drl@gmail.com>
2023-09-08 00:31:56 -0400

    Update standaloen

--- Experiments/RealWorldRigid_DataLoaderStandalone.py
+++ Experiments/RealWorldRigid_DataLoaderStandalone.py
@@ -79,6 +79,7 @@ class RealWorldRigid_PreDataset(object):
 
 		# Set files. 
 		self.setup()
+		self.set_ground_tag_pose_dict()
 
 		self.stat_dir_name='RealWorldRigid'
 				
@@ -399,7 +400,7 @@ class RealWorldRigid_PreDataset(object):
 			# For each task, save task_file_list to One numpy. 
 			suffix = ""
 			# if self.args.images_in_real_world_dataset:
-			suffix = "_wImages"
+			suffix = "_NewRelPose"
 			task_numpy_path = os.path.join(self.dataset_directory, self.task_list[task_index], "New_Task_Demo_Array{0}.npy".format(suffix))
 			np.save(task_numpy_path, self.task_demo_array)
 
