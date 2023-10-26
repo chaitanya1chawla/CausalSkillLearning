91b3316647df2dd8ae0417c23da5ed1999fddaf4
tanmayshankar <tanmay.shankar@gmail.com>
2023-10-26 13:04:13 -0400

    Debug

--- Experiments/PolicyManagers.py
+++ Experiments/PolicyManagers.py
@@ -2990,6 +2990,9 @@ class PolicyManager_Pretrain(PolicyManager_BaseClass):
 
 		# Relabel the dims. 
 
+		print("Debug in Relabel")
+		embed()
+
 		torchified_object_state = torch.from_numpy(self.normalized_subsampled_relative_object_state).to(device).view(-1, self.args.batch_size, self.args.env_state_size)		
 		relabelled_state_sequence[..., -self.args.env_state_size:] = torchified_object_state
 
